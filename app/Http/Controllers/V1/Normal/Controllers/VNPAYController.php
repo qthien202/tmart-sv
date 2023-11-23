0bada9edce4121d0b254b3d63a3ee3ea7947a207
Thanh Trần <thanhtran2002123@gmail.com>
2023-11-23 17:38:17 +0700

    fixed 2

--- app/Http/Controllers/V1/Normal/Controllers/VNPAYController.php
+++ app/Http/Controllers/V1/Normal/Controllers/VNPAYController.php
@@ -89,14 +89,24 @@ class VNPAYController extends BaseController
     // }
 
     public function vnpayIPNReturn(Request $rq){
-        // $inputData = $rq->all();
-        // $orderId = $inputData['vnp_TxnRef'];
+        $inputData = $rq->all();
+        $orderId = $inputData['vnp_TxnRef'];
 
-        // $order = Order::find($orderId);
-        // $orderPayment = $order->orderPayments;
-        // $orderPayment["payment_status"] = "test"; 
-        // $orderPayment->save();
+        
+
+
+        $order = Order::find($orderId);
+        $orderPayment = $order->orderPayments;
+        $orderPayment["payment_status"] = "test"; 
+        $orderPayment->save();
+        $returnData['RspCode'] = '00';
+        $returnData['Message'] = 'Confirm Success';
+        echo json_encode($returnData);
+        return;
         // dd($orderPayment['payment_status']);
+        // $vnp_Amount = $inputData['vnp_Amount']/100;
+        // dd($orderPayment["amount"] == $vnp_Amount);
+
 
         /* Payment Notify
         * IPN URL: Ghi nhận kết quả thanh toán từ VNPAY
