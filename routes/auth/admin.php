0130d24ac1cc7fc09d5141bc2945e893819c25cc
Thanh Trần <thanhtran2002123@gmail.com>
2024-06-09 06:22:35 +0700

    không chặn admin get manufacturer

--- routes/auth/admin.php
+++ routes/auth/admin.php
@@ -17,7 +17,7 @@ $api->group(['middleware' => ['cors', 'trimInput', 'verifySecret', 'authorize','
     $api->put('/banners/{id:[0-9]+}', 'BannerController@update');
     $api->delete('/banners/{id:[0-9]+}', 'BannerController@delete');
     // Manufacturers
-    $api->get('/get_manufacturers', 'ManufacturerController@getManuFacturers');
+    // $api->get('/get_manufacturers', 'ManufacturerController@getManuFacturers');
     $api->post('/create_manufacturer', 'ManufacturerController@createManuFacturer');
     $api->get('/get_manufacturer_by_id/{id}', 'ManufacturerController@getManuFacturerById');
     $api->put('/update_manufacturer/{id}', 'ManufacturerController@updateManuFacturer');
