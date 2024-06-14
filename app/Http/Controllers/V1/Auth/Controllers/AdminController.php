f8487d3c8caafa30d5d961a30e2adcaa22063a71
Thanh Trần <thanhtran2002123@gmail.com>
2024-06-14 22:50:45 +0700

    chart và tổng order

--- app/Http/Controllers/V1/Auth/Controllers/AdminController.php
+++ app/Http/Controllers/V1/Auth/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\V1\Auth\Models\Order;
 use App\Http\Controllers\V1\Auth\Models\Product;
 use App\SERVICE;
 use App\User;
+use Carbon\Carbon;
 
 class AdminController extends BaseController
 {
@@ -18,7 +19,8 @@ class AdminController extends BaseController
 
     public function statistical(){
         $numUser = User::where('role_id',2)->where('is_active',1) ->count();
-        $numOrder = Order::where('status_code','delivered')->count();
+        // $numOrder = Order::where('status_code','delivered')->count();
+        $numOrder = Order::count();
         $order = Order::where('status_code','delivered')->get();
         $totalRevenue = 0;
         foreach ($order as $item) {
@@ -36,6 +38,38 @@ class AdminController extends BaseController
             'revenue'=>$totalRevenue,
             'view' =>$views,
         ];
+        return $this->responseSuccess(null,$data);
+    }
+
+    public function chartData(){
+        // Order::where('status_code','delivered')
+        Carbon::setLocale('vi');
+        $timeNow = Carbon::now();
+        $time =$timeNow->subWeek();
+        $timeStr = $time->toDateString();
+        $orders = Order::where('order_date','>=',$timeStr)->get();
+        $revenueDate = [];
+        for ($i=0; $i < 7; $i++) { 
+            // Chưa tối ưu
+            $date =  Carbon::now()->subWeek()->addDays($i);
+            $dateStr = $date->toDateString();
+            $revenueDate[$dateStr] = ["revenue"=>0,"profit"=>0];
+        }
+        // [Doanh thu, loi nhuan (test)]
+        foreach ($orders as $order) {
+            $orderDate = $order->order_date;
+            array_filter($order->info_total_amount,function($a) use (&$revenueDate,$orderDate){
+                if($a['code'] == "total"){
+                    $revenueDate[$orderDate]["revenue"]+=(int)($a['value']);
+                    $revenueDate[$orderDate]["profit"]+=$a['value'] > 10000 ? (int)(0.95*$a['value']):0;
+                }
+            });
+        }
+        $data = [];
+        foreach ($revenueDate as $key => $value) {
+            $data[] = ["date"=>$key,"revenue"=>$value["revenue"],"profit"=>$value["profit"]];
+        }
+
         return $this->responseSuccess(null,$data);
     }
 
