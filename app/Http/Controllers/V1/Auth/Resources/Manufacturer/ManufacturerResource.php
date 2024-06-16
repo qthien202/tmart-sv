b2a5c5b334117716d9d364826e2ab081540374c5
Thanh Trần <thanhtran2002123@gmail.com>
2024-06-16 10:26:07 +0700

    manufaturer them created va updated

--- app/Http/Controllers/V1/Auth/Resources/Manufacturer/ManufacturerResource.php
+++ app/Http/Controllers/V1/Auth/Resources/Manufacturer/ManufacturerResource.php
@@ -29,6 +29,8 @@ class ManufacturerResource extends BaseResource
                 'field' => $this->field,
                 'year_established' => $this->year_established,
                 'product_infomation' => $this->product_infomation,
+                'created_at' => !empty($this->created_at) ? date('Y-m-d H:i:s', strtotime($this->created_at)) : null,
+                'updated_at' => !empty($this->updated_at) ? date('Y-m-d H:i:s', strtotime($this->updated_at)) : null,
             ];
         } catch (\Exception $ex) {
             $response = SERVICE_Error::handle($ex);
